57fba31d1382018d2e3611343451293c14966b57
Antonio Rodriguez <antonioinorge@hotmail.com>
2024-11-15 16:31:42 +0100

    Refactoring

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ OPTIONS.add_experimental_option("detach", True)
 DRIVER = webdriver.Chrome(options = OPTIONS)
 DRIVER.get("http://orteil.dashnet.org/experiments/cookie/")
 COOKIE = DRIVER.find_element(By.ID, "cookie")
-COOKIE_COUNTER = DRIVER.find_element(By.ID, "money").text
-STORE = DRIVER.find_element(By.ID, "store")
-ITEMS = DRIVER.find_elements(by=By.CSS_SELECTOR, value="#store div")
+COOKIE_COUNTER = int(DRIVER.find_element(By.ID, "money").text)
 STORE_ITEMS = DRIVER.find_element(By.ID, "store").text.strip().split()
+ITEMS = DRIVER.find_elements(by=By.CSS_SELECTOR, value="#store div")
 START = time.time()
-PAUSE = 5
-
-item_ids = [item.get_attribute("id") for item in ITEMS]
-timeout = 0
+FIVE_SECONDS = 5
 
 def store_prices():
     prices = []
@@ -28,21 +24,19 @@ def store_prices():
 def click_cookie():
     COOKIE.click()
 
+item_ids = [item.get_attribute("id") for item in ITEMS]
 
 #TODO: Start from scratch
-prices_sorted = store_prices()
-cookie_upgrades = {}
-for n in range(len(prices_sorted)):
-    cookie_upgrades[prices_sorted[n]] = item_ids[n]
-print(cookie_upgrades)
 
 while True:
     click_cookie()
-    # if time.time() > START + PAUSE:
-    # if timeout == PAUSE:
-    #     for price, upgrade_id in cookie_upgrades.items():
-    #         if int(COOKIE_COUNTER) >= price:
-    #             DRIVER.find_element(By.ID, upgrade_id).click()
-    #             timeout = 0
+    if time.time() > START + FIVE_SECONDS:
+        cookie_upgrades = {}
+        for n in range(len(store_prices())):
+            cookie_upgrades[store_prices()[n]] = item_ids[n]
+        for price, item_id in cookie_upgrades.items():
+            if COOKIE_COUNTER >= price:
+                DRIVER.find_element(By.ID, item_id).click()
+
 
 # driver.close()
