fa9cbbf288bcbfe746e4601d27b1a60c95aa16f0
Antonio Rodriguez <antonioinorge@hotmail.com>
2024-11-15 11:23:05 +0100

    add Selenium snippet

---
+++ main.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options = options)
+
+driver.get("URL")
+
+driver.close()
