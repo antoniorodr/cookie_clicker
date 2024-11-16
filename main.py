c0a5fb037045f3c680340cb4177b3a4cb6307dbf
Antonio Rodriguez <antonioinorge@hotmail.com>
2024-11-16 09:27:24 +0100

    Small fixes

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ OPTIONS.add_experimental_option("detach", True)
 DRIVER = webdriver.Chrome(options = OPTIONS)
 DRIVER.get("http://orteil.dashnet.org/experiments/cookie/")
 COOKIE = DRIVER.find_element(By.ID, "cookie")
-COOKIE_COUNTER = int(DRIVER.find_element(By.ID, "money").text)
 START = time.time()
 FIVE_SECONDS = 5
 
@@ -30,13 +29,14 @@ def click_cookie():
 while True:
     click_cookie()
     if time.time() > START + FIVE_SECONDS:
+        cookie_counter = int(DRIVER.find_element(By.ID, "money").text)
         cookie_upgrades = {}
         for n in range(len(store_prices())):
             items = DRIVER.find_elements(by=By.CSS_SELECTOR, value="#store div")
             item_ids = [item.get_attribute("id") for item in items]
             cookie_upgrades[store_prices()[n]] = item_ids[n]
         # for price, item_id in cookie_upgrades.items():
-            # if COOKIE_COUNTER >= price:
+            # if cookie_counter >= price:
         print(cookie_upgrades)
                 # DRIVER.find_element(By.ID, item_id).click()
 
